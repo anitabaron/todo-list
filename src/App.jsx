27d4a4cbb44d04d95b885c4da927da1ffd1bd199
anitabaron <111062951+anitabaron@users.noreply.github.com>
2025-01-07 12:15:07 +0100

    store config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl">Hello, Tailwind CSS!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Uwzględnij wszystkie pliki w folderze `src`
+    './public/index.html', // Opcjonalnie dodaj plik `index.html`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
